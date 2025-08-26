9756dc543eb67ce498afee4f2a26a57374ca033b
viraj-vvk <viraj.vvk@gmail.com>
2025-08-26 12:59:37 +0530

    Refactored SimpleCaching

--- SimpleCaching.java
+++ SimpleCaching.java
@@ -7,100 +7,101 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
-class SimpleCache<K, V> {
-    private final ConcurrentHashMap<K, Entry<V>> cache;
-    private final AtomicInteger size;
-    private final int maxSize;
-    private final long defaultTtl;
+public class SimpleCaching {
 
-    private record Entry<V>(V value, long expiry) {
+    private static class SimpleCache<K, V> {
+        private final ConcurrentHashMap<K, Entry<V>> cache;
+        private final AtomicInteger size;
+        private final int maxSize;
+        private final long defaultTtl;
 
-        private Entry(V value, long expiry) {
-            this.value = value;
-            this.expiry = Instant.now().plusSeconds(expiry).toEpochMilli();
+        private record Entry<V>(V value, long expiry) {
+
+            private Entry(V value, long expiry) {
+                this.value = value;
+                this.expiry = Instant.now().plusSeconds(expiry).toEpochMilli();
+            }
+
+            public boolean isExpired() {
+                return System.currentTimeMillis() > expiry;
+            }
+
+            @Override
+            public String toString() {
+                return value.toString();
+            }
         }
 
-        public boolean isExpired() {
-            return System.currentTimeMillis() > expiry;
+        public SimpleCache(int maxSize, long defaultTtl) {
+            if (maxSize < 1 || defaultTtl < 1) {
+                throw new IllegalArgumentException("Invalid params passed");
+            }
+            this.cache = new ConcurrentHashMap<>();
+            this.size = new AtomicInteger(0);
+            this.maxSize = maxSize;
+            this.defaultTtl = defaultTtl;
+        }
+
+        public void put(K key, V value) {
+            this.put(key, value, defaultTtl);
+        }
+
+        public void put(K key, V value, long defaultTtl) {
+            if (Objects.isNull(key) || defaultTtl < 1) {
+                throw new IllegalArgumentException("Invalid values passed");
+            }
+            if (size.get() >= maxSize) {
+                evictOldest();
+            }
+            if (Objects.isNull(cache.put(key, new Entry<>(value, defaultTtl)))) {
+                size.incrementAndGet();
+            }
+        }
+
+        private void evictOldest() {
+            long max = Long.MAX_VALUE;
+            K oldestKey = null;
+
+            for (Map.Entry<K, Entry<V>> entry : cache.entrySet()) {
+                if (entry.getValue().expiry() < max) {
+                    oldestKey = entry.getKey();
+                    max = entry.getValue().expiry();
+                }
+            }
+
+            if (Objects.nonNull(oldestKey)) {
+                cache.remove(oldestKey);
+                size.decrementAndGet();
+            }
+        }
+
+        public V get(K key) {
+            Entry<V> value = cache.get(key);
+            if (Objects.nonNull(value)) {
+                if (!value.isExpired()) {
+                    return value.value();
+                }
+                cache.remove(key);
+                size.decrementAndGet();
+            }
+            return null;
+        }
+
+        public Set<Map.Entry<K, V>> entrySet() {
+            return cache.entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey, entry -> entry.getValue().value())).entrySet();
         }
 
         @Override
         public String toString() {
-            return value.toString();
+            return "SimpleCache{" +
+                    "cache=" + cache +
+                    ", size=" + size +
+                    ", maxSize=" + maxSize +
+                    ", defaultTtl=" + defaultTtl +
+                    '}';
         }
     }
 
-    public SimpleCache(int maxSize, long defaultTtl) {
-        if (maxSize < 1 || defaultTtl < 1) {
-            throw new IllegalArgumentException("Invalid params passed");
-        }
-        this.cache = new ConcurrentHashMap<>();
-        this.size = new AtomicInteger(0);
-        this.maxSize = maxSize;
-        this.defaultTtl = defaultTtl;
-    }
-
-    public void put(K key, V value) {
-        this.put(key, value, defaultTtl);
-    }
-
-    public void put(K key, V value, long defaultTtl) {
-        if (Objects.isNull(key) || defaultTtl < 1) {
-            throw new IllegalArgumentException("Invalid values passed");
-        }
-        if (size.get() >= maxSize) {
-            evictOldest();
-        }
-        if (Objects.isNull(cache.put(key, new Entry<>(value, defaultTtl)))) {
-            size.incrementAndGet();
-        }
-    }
-
-    private void evictOldest() {
-        long max = Long.MAX_VALUE;
-        K oldestKey = null;
-
-        for (Map.Entry<K, Entry<V>> entry : cache.entrySet()) {
-            if (entry.getValue().expiry() < max) {
-                oldestKey = entry.getKey();
-                max = entry.getValue().expiry();
-            }
-        }
-
-        if (Objects.nonNull(oldestKey)) {
-            cache.remove(oldestKey);
-            size.decrementAndGet();
-        }
-    }
-
-    public V get(K key) {
-        Entry<V> value = cache.get(key);
-        if (Objects.nonNull(value)) {
-            if (!value.isExpired()) {
-                return value.value();
-            }
-            cache.remove(key);
-            size.decrementAndGet();
-        }
-        return null;
-    }
-
-    public Set<Map.Entry<K, V>> entrySet() {
-        return cache.entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey, entry -> entry.getValue().value())).entrySet();
-    }
-
-    @Override
-    public String toString() {
-        return "SimpleCache{" +
-                "cache=" + cache +
-                ", size=" + size +
-                ", maxSize=" + maxSize +
-                ", defaultTtl=" + defaultTtl +
-                '}';
-    }
-}
-
-public class SimpleCaching {
     public static void main(String[] args) {
         final Random random = new Random();
         final SimpleCache<Integer, Integer> simpleCache = new SimpleCache<>(10, 10);
@@ -109,7 +110,7 @@ public class SimpleCaching {
                 Thread.sleep(intVal * 1000L);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
-            }
+            }https://tacnique.com/interviews/9fb1ad15-5ac1-4d6c-8a03-28f2a1ecd461/questions/9e75917a-efe9-40fc-b35e-d58fef2eb259
             simpleCache.put(intVal, random.nextInt(), random.nextInt(5, 10));
         });
         for (Map.Entry<Integer, Integer> entry : simpleCache.entrySet()) {
